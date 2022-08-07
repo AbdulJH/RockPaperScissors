32235c1a4d221e4898e13b9b6f9d615834d6f44f
Abdul Hussein <abdul.chs21@gmail.com>
2022-08-07 17:33:23 -0500

    Created a playround function that shows results based on who won RPS

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function getComputerChoice() {
-  const computerChoices = ["Rock", "Paper", "Scissors"];
+  const computerChoices = ["rock", "paper", "scissors"];
   const randomChoice = computerChoices[Math.floor(Math.random()*computerChoices.length)];
   return randomChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection == computerSelection) {
+    return (`Tie! Both have picked ${playerSelection}`);
+  }
+
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return ("You Win! Rock beats Scissors");
+  } else if (playerSelection === "rock" && computerSelection !== "scissors"){
+    return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }
+
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    return ("You Win! Paper beats Rock");
+  } else if (playerSelection === "paper" && computerSelection !== "rock"){
+    return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }
+
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+    return ("You Win! Scissors beats Paper");
+  } else if (playerSelection === "scissors" && computerSelection !== "paper"){
+    return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }
+
+}
+
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice();
+
+
+
