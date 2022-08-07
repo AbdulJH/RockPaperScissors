9250f90f626eb371608fc4c4b909d56da126e99b
Abdul Hussein <abdul.chs21@gmail.com>
2022-08-07 17:39:45 -0500

    Used more template literals and removed uncessary if/else statements

--- script.js
+++ script.js
@@ -14,26 +14,19 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === "rock" && computerSelection === "scissors") {
     return ("You Win! Rock beats Scissors");
-  } else if (playerSelection === "rock" && computerSelection !== "scissors"){
-    return (`You Lose! ${computerSelection} beats ${playerSelection}`);
-  }
-
-  if (playerSelection === "paper" && computerSelection === "rock") {
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
     return ("You Win! Paper beats Rock");
-  } else if (playerSelection === "paper" && computerSelection !== "rock"){
-    return (`You Lose! ${computerSelection} beats ${playerSelection}`);
-  }
-
-  if (playerSelection === "scissors" && computerSelection === "paper") {
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
     return ("You Win! Scissors beats Paper");
-  } else if (playerSelection === "scissors" && computerSelection !== "paper"){
-    return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+  } else {
+    return (`You Lose!! ${computerSelection} beats ${playerSelection}`);
   }
 
 }
-
-const playerSelection = "Paper";
+const playerSelection = "ROCK";
 const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
 
 
 
