95ade29405b4ab029786423663c1a033247df1a7
Abdul Hussein <abdul.chs21@gmail.com>
2022-08-07 21:59:56 -0500

    Created a game function that goes through 5 rounds and asks for user input for each of them. Displays current score and shows winner at end of game

--- script.js
+++ script.js
@@ -4,29 +4,49 @@ function getComputerChoice() {
   return randomChoice;
 }
 
+let userScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
 
   if (playerSelection == computerSelection) {
-    return (`Tie! Both have picked ${playerSelection}`);
+    return (`Tie! Both picked ${playerSelection}`);
   }
 
   if (playerSelection === "rock" && computerSelection === "scissors") {
+    userScore++;
     return ("You Win! Rock beats Scissors");
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    userScore++;
     return ("You Win! Paper beats Rock");
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    userScore++;
     return ("You Win! Scissors beats Paper");
   } else {
+    computerScore++;
     return (`You Lose!! ${computerSelection} beats ${playerSelection}`);
   }
 
 }
-const playerSelection = "ROCK";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
+function game() {
+  for (let i=0; i<5; i++) {
+    let playerSelection = prompt("Rock, Paper or Scissors");
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(`Score: ${userScore}:${computerScore}`);
+  }
+  if (userScore > computerScore) {
+    console.log(`You Win! Score: ${userScore}:${computerScore}`);
+  } else if (userScore < computerScore) {
+    console.log(`You Lose! Score: ${userScore}:${computerScore}`);
+  } else {
+    console.log(`You Tied! SCore: ${userScore}:${computerScore}`);
+  }
+}
+game();
 
 
 
